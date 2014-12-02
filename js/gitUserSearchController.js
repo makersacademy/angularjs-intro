fbb25ba4d7289bb074affe79c85c7214cf2657ba
Roi Driscoll <roi.driscoll@gmail.com>
2014-12-02 17:24:47 +0000

    Replaced dummy data in doSearch method to make the call to Github API.

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,17 +1,10 @@
-githubUserSearch.controller('GitUserSearchController', function($scope) {
+githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
+
+  var searchResource = $resource('https://api.github.com/search/users');
 
   $scope.doSearch = function (){
-    console.log($scope.searchTerm);
-    $scope.searchResult = {
-      items: [{
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      }, {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }]
-    };
+    $scope.searchResult = searchResource.get({
+      q: $scope.searchTerm
+    });
   };
 });
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -17,6 +17,16 @@ describe('GitUserSearchController', function() {
 
   describe('when searching for a user', function() {
 
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond({
+          items: items
+        });
+    }));
+
     var items = [{
       "login": "tansaku",
       "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
@@ -30,6 +40,8 @@ describe('GitUserSearchController', function() {
     it('should display search results', function() {
       scope.searchTerm = 'hello';
       scope.doSearch();
+      scope.$apply();
+      httpBackend.flush();
       expect(scope.searchResult.items).toEqual(items);
     });
   });
