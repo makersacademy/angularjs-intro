1ed66b9cda1e05a88f2a90ec6385538bb93d48d3
Roi Driscoll <roi.driscoll@gmail.com>
2014-12-02 15:30:10 +0000

    added dummy API data

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,17 +1,34 @@
 describe('GitUserSearchController', function() {
-    beforeEach(module('GitUserSearch'));
+  beforeEach(module('GitUserSearch'));
 
-    var scope, ctrl;
+  var scope, ctrl;
 
-    beforeEach(inject(function($rootScope, $controller) {
-        scope = $rootScope.$new();
-        ctrl = $controller('GitUserSearchController', {
-            $scope: scope
-        });
-    }));
-
-    it('should initialise with an empty search result and term', function() {
-      expect(scope.searchResult).toBeUndefined();
-      expect(scope.searchTerm).toBeUndefined();
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    ctrl = $controller('GitUserSearchController', {
+      $scope: scope
     });
+  }));
+
+  it('should initialise with an empty search result and term', function() {
+    expect(scope.searchResult).toBeUndefined();
+    expect(scope.searchTerm).toBeUndefined();
+  });
+
+  describe('when searching for a user', function() {
+
+    var items = [{
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    }, {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }];
+
+    it('should display search results', function() {
+      expect(scope.searchResult.items).toEqual(items);
+    });
+  });
 });
