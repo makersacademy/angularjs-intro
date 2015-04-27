a1c7ab0221d575e7a76a540042facf104a3c26b8
DanLDB <dpbergquist>
2015-04-27 13:17:37 +0100

    add a controller

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,15 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }));
+
+  it('initialises with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  });
+  
+});
