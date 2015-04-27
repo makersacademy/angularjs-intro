938c5273d7037a3a44ad455cca7eb6057ba3f492
DanLDB <dpbergquist>
2015-04-27 13:35:13 +0100

    add mock data to controller searchResults

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -11,5 +11,25 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
-  
+
+  describe('when searching for a user', function() {
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      }, 
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it('displays search results', function() {
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
+
 });
